1575bf93dbe7ed091b55c30aeb94e7fd3fad808f
Daniel <daniel.ospinaperez@gmail.com>
2018-02-19 14:14:06 -0500

    prueba resultados

--- menus.js
+++ menus.js
@@ -2,7 +2,7 @@ Menu = require("./models/model.js");
 
 function startMenu(menuName, channel, member) {
     Menu.findByName(menuName, function(err, menu) {
-        console.log(menu);
+        console.log(menu.data);
     });
 }
 
