fe1dfab7eea2636f0d3398ed73440f39b9a466db
Daniel <daniel.ospinaperez@gmail.com>
2018-02-19 14:44:18 -0500

    prueba log no encuentra datos

--- menus.js
+++ menus.js
@@ -2,7 +2,10 @@ Menu = require("./models/model.js");
 
 function startMenu(menuName, channel, member) {
     Menu.findByName(menuName, function(err, menu) {
-        console.log(menu.data);
+        if (menu.data.length == 0) {
+            console.log(Menu not found.);
+            return;
+        }
     });
 }
 
