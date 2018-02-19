3146a4ebd5a24ecb6dbe5f46123afc55587d9efd
Daniel <daniel.ospinaperez@gmail.com>
2018-02-19 14:50:05 -0500

    faltaron comillas

--- menus.js
+++ menus.js
@@ -3,7 +3,7 @@ Menu = require("./models/model.js");
 function startMenu(menuName, channel, member) {
     Menu.findByName(menuName, function(err, menu) {
         if (menu.data.length == 0) {
-            console.log(Menu not found.);
+            console.log("Menu not found.");
             return;
         }
     });
