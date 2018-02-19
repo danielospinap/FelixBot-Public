b74974412d7a78cd42f3819048dc0aa610378cb0
Daniel <daniel.ospinaperez@gmail.com>
2018-02-19 17:09:44 -0500

    prueba parseo de emoji

--- menus.js
+++ menus.js
@@ -12,22 +12,40 @@ function startMenu(menuName, channel, member) {
 }
 
 function showMenu(menu, channel, member) {
-    msg = parseStatement(menu.data[0].questions[0].q.statement, member);
-    channel.send(msg);
+    statement = parseMember(menu.data[0].questions[0].q.statement, member);
+    channel.send(statement);
 }
 
 function parseStatement(text, member) {
+    statement = parseMember(text, member);
+    statement = parseEmoji(statement, member);
+}
+
+function parseMember(text, member) {
     var parts = text.split("@member");
     var parsedText = parts[0];
     for (var i = 1; i < parts.length; i++) {
         parsedText = parsedText + member + parts[i];
     }
 
-    parsedText = parsedText.split(/:\w+:/);
     console.log(parsedText);
+    return parsedText;
+}
 
+function parseEmoji(text, member) {
+    var emojis = member.guild.emojis;
+    var parts = text.split(/:\w+:/);
+    var parsedText = parts[0];
 
-    return parsedText
+    for (var i = 1; i < array.length; i++) {
+        var i = text.search(/:\w+:/);
+        var f = text.substring(i+1).search(":");
+
+        parsedText = parsedText + emojis.find('name', text.substring(i+1, f+i+1))
+    }
+
+    console.log(parsedText);
+    return parsedText;
 }
 
 module.exports = startMenu;
